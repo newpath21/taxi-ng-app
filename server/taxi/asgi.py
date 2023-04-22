1665baba7b279e39edcf0224428570b924396093
askar <asinible@gmail.com>
2023-04-22 18:08:17 +0600

    models, settings created

---
+++ server/taxi/asgi.py
@@ -0,0 +1,22 @@
+"""
+ASGI config for taxi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+from channels.routing import ProtocolTypeRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxi.settings')
+
+django_asgi_application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_application
+})
