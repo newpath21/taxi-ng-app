135e5d3fa4bbfd99ac2d912f642903ea69dfcf5e
askar <asinible@gmail.com>
2023-05-02 00:45:37 +0600

    sign-up and log-in forms (ng-client)

--- client/src/app/components/sign-up/sign-up.component.spec.ts
+++ client/src/app/components/sign-up/sign-up.component.spec.ts
@@ -1,23 +1,57 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule, HttpTestingController
+} from "@angular/common/http/testing";
 
-import { SignUpComponent } from './sign-up.component';
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { Router } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import { FormsModule } from "@angular/forms";
 
-describe('SignUpComponent', () => {
+import { AuthService } from "src/app/services/auth.service";
+import { createFakeUser } from "src/app/testing/factories";
+import { SignUpComponent } from "./sign-up.component";
+
+fdescribe('SignUpComponent', () => {
   let component: SignUpComponent;
   let fixture: ComponentFixture<SignUpComponent>;
+  let router: Router;
+  let httpMock: HttpTestingController;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SignUpComponent ]
-    })
-    .compileComponents();
-
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([])
+      ],
+      declarations: [
+        SignUpComponent
+      ],
+      providers: [
+        AuthService
+      ]
+    });
     fixture = TestBed.createComponent(SignUpComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    router = TestBed.inject(Router);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should allow a user to sign up for an account', () => {
+    const spy = spyOn(router, 'navigateByUrl');
+    const user = createFakeUser();
+    const photo = new File(['photo'], user.photo, { type: 'image/jpeg' });
+    component.user = {
+      username: user.username,
+      firstName: user.first_name,
+      lastName: user.last_name,
+      password: 'pAssw0rd!',
+      group: user.group,
+      photo
+    };
+    component.onSubmit();
+    const request = httpMock.expectOne('/api/sign_up/');
+    request.flush(user);
+    expect(spy).toHaveBeenCalledWith('/log-in');
   });
 });
